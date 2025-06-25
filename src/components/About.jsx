beaca53c013b1aa9a401a0e3f89c6eb3d99afd0a
Omar Elderwy <omarderwy@gmail.com>
2025-06-26 00:27:19 +0300

    adjust tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,17 +8,19 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    className='xs:w-[250px] w-full'
+    tiltReverse={false}
+    tiltMaxAngleX={20}
+    tiltMaxAngleY={20}
+    scale={1.1}
+    transitionSpeed={250}
+   >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
         <img
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,11 +19,11 @@ const ProjectCard = ({
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+        tiltReverse={false}
+        tiltMaxAngleX={20}
+        tiltMaxAngleY={20}
+        scale={1.1}
+        transitionSpeed={250}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
